11fc839ffb771d2e9b011dbaf73c845c76455c8d
jose <hernandezjos1121@gmail.com>
2023-06-12 14:13:08 -0400

    second commit

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ import Nav from "./components/Nav"
 import Landing from './components/Landing'
 import Highlights from './components/Highlights';
 import Featured from './components/Featured';
+import Discounted from './components/Discounted';
+import Explore from './components/Explore';
+import Footer from './components/Footer';
+
+
+
+
+
 function App(){
   return (
     <div className='App'>
@@ -10,6 +18,10 @@ function App(){
       <Landing />
       <Highlights/>
       <Featured/>
+      <Discounted/>
+      <Explore/>
+      <Footer/>
+
 
     </div>
     
